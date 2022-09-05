686644e68be5fa973d99976b72556010cdfc8961
Kevin Callon <kevincallon@gmail.com>
2022-09-05 11:52:27 +0200

    gif width increased and made conditional, column amount fixed at 3 and max width of container set to 1100px

--- src/components/RowOfGifs.js
+++ src/components/RowOfGifs.js
@@ -8,21 +8,34 @@ const RowOfGifs = ({ gifs, runApiSearch }) => {
   const { width, height } = useWindowSize();
   // const [gifWidths, setGifWidths] = useState([]);
   // const [gifsContainerHeight, setGifsContainerHeight] = useState(null);
-  const [gifsStyles, setGifsStyles] = useState({ width: 0, columnCount: 0, WebkitColumnCount: 0, MozColumnCount: 0 });
+  const [gifWidth, setGifWidth] = useState(0);
+  // const [gifsStyles, setGifsStyles] = useState({ width: 0, columnCount: 0, WebkitColumnCount: 0, MozColumnCount: 0 });
+
 
   // const updateScrollX = () => setScrollX(gifsContainerRef.current.scrollLeft);
 
-  const calculateGifsColumns = () => {
-    // const gifsContainerWidth = gifsContainerRef.current.style.width;
-    const availableWidth = width * 0.9;
-    const rowGap = 5;
-    const gifWidth = 200;
-    const totalGifWidth = rowGap + gifWidth;
-    const columnAmount = Math.floor((availableWidth + rowGap) / totalGifWidth);
-    const gifsWidth = (columnAmount * totalGifWidth) - rowGap;
-    if (columnAmount && gifsWidth) {
-      setGifsStyles({ width: gifsWidth, columnCount: columnAmount, WebkitColumnCount: columnAmount, MozColumnCount: columnAmount });
+  // const calculateGifsColumns = () => {
+  //   const availableWidth = width * 0.9;
+  //   const columnGap = 5;
+  //   const gifWidth = 300;
+  //   const totalGifWidth = columnGap + gifWidth;
+  //   const columnAmount = Math.floor((availableWidth + columnGap) / totalGifWidth);
+  //   const gifsWidth = (columnAmount * totalGifWidth) - columnGap;
+  //   if (columnAmount && gifsWidth) {
+  //     setGifsStyles({ width: gifsWidth, columnCount: columnAmount, WebkitColumnCount: columnAmount, MozColumnCount: columnAmount });
+  //   }
+  // };
+
+  const calculateGifWidth = () => {
+    let availableWidth = width * 0.9;
+    if (availableWidth > 1200) {
+      availableWidth = 1200;
     }
+    const columns = 3;
+    const columnGap = 5;
+    const columnGapsTotal = (columnGap - 1) * columns;
+    const individualgifWidth = (availableWidth - columnGapsTotal) / columns;
+    setGifWidth(individualgifWidth);
   };
 
   const infiniteScroll = () => {
@@ -32,28 +45,36 @@ const RowOfGifs = ({ gifs, runApiSearch }) => {
     //   const gifsContainerHeight = gifsContainerRef.current?.clientHeight;
     //   console.log('gifsContainerHeight: ', gifsContainerHeight);
     // };
+    const refEl = gifsContainerRef.current;
 
-    if (gifsContainerRef.current?.scrollTop + gifsContainerRef.current?.clientHeight >= gifsContainerRef.current?.scrollHeight) {
+    // console.log('refEl?.scrollTop: ', refEl?.scrollTop);
+    // console.log('refEl?.clientHeight: ', refEl?.clientHeight);
+    // console.log('refEl?.scrollHeight: ', refEl?.scrollHeight);
+
+    if (refEl?.scrollTop + refEl?.clientHeight >= refEl?.scrollHeight - 20) {
       runApiSearch();
     }
   };
 
-  useEffect(calculateGifsColumns, [width]);
-  // useEffect(infiniteScroll, [gifs.length, gifsContainerRef]);
+  // useEffect(calculateGifsColumns, [width]);
+  useEffect(calculateGifWidth, [width]);
+
 
 
   return (
     <div className='gifs-container' ref={gifsContainerRef} onScroll={infiniteScroll}>
       <div
         className="gifs"
-        style={gifsStyles}
+      // style={gifsStyles}
       >
         {gifs.map((gif, index) => (
           <img
             key={index}
             alt={gif.title}
             className="gif"
-            src={gif.images.fixed_width_downsampled.url}
+            style={{ width: gifWidth }}
+            // src={gif.images.fixed_width_downsampled.url}
+            src={gif.images.fixed_width.url}
             onClick={() => window.open(gif.embed_url, '_blank')}
           />
         ))}
--- src/components/TrendingGifs.js
+++ src/components/TrendingGifs.js
@@ -17,6 +17,7 @@ export default function TrendingGifs() {
         if (jsonRes.meta.status < 200 || jsonRes.meta.status > 299) {
           throw new Error(jsonRes.meta.msg);
         } else {
+          console.log('jsonRes.data: ', jsonRes.data);
           const gifsToSet = offset === 0 ? jsonRes.data : trendingGifs.concat(jsonRes.data);
           // console.log('gifsToSet: ', gifsToSet);
           setTrendingGifs(gifsToSet);
