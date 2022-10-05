26d5301133e840ce989fe2a58ea2ba5928fd5b44
Kevin Callon <kevincallon@gmail.com>
2022-10-05 16:11:37 +0200

    top bar border bottom width and footer height accounted for in gif container height

--- src/components/gif.js
+++ src/components/gif.js
@@ -6,13 +6,13 @@ export function Gif({ gifObject, isLowResolution, playOnlyOnHover }) {
   const [src, setSrc] = useState('');
   const [isInViewport, setIsInViewport] = useState(true); // true as default so that visible gifs are immediately active on load
 
-  const resCondition = isLowResolution ? images.fixed_height_downsampled.url : images.fixed_height.url;
+  const properResolutionUrl = isLowResolution ? images.fixed_height_downsampled.url : images.fixed_height.url;
 
   const handleClick = () => window.open(gifObject.embed_url, '_blank');
 
   const handleMouseEnter = () => {
     if (playOnlyOnHover) {
-      setSrc(resCondition);
+      setSrc(properResolutionUrl);
     }
   };
 
@@ -26,7 +26,7 @@ export function Gif({ gifObject, isLowResolution, playOnlyOnHover }) {
     const windowHeight = window.innerHeight;
     // const boundaryTop = 50 + (windowHeight * 0.1); // 50px search bar plus 10vh padding 
     const boundaryTop = 70;
-    const boundaryBottom = windowHeight * 0.95; // 5vh margin bottom
+    const boundaryBottom = windowHeight * 0.97; // excludes 3vh footer
     const { top: gifTop, bottom: gifBottom } = gifRef.current.getBoundingClientRect();
     const isInBounds = gifBottom > boundaryTop && gifTop < boundaryBottom;
     setIsInViewport(isInBounds);
@@ -44,7 +44,7 @@ export function Gif({ gifObject, isLowResolution, playOnlyOnHover }) {
     if (playOnlyOnHover) {
       url = images.fixed_height_still.url;
     } else {
-      url = !isInViewport ? images.fixed_height_still.url : resCondition;
+      url = !isInViewport ? images.fixed_height_still.url : properResolutionUrl;
     }
     setSrc(url);
   };
