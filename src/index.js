396098a153810700474ef14a1ed434663a9e336f
Kevin Callon <kevincallon@gmail.com>
2023-04-03 11:57:20 +0200

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import { App } from './components/app/App';
 
 const root = createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
-);
+root.render(<App />);
 
 
