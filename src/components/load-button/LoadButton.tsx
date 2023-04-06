bb29661fa95f2cbc329b23e203c2d766df27e7e0
Kevin Callon <kevincallon@gmail.com>
2023-04-06 13:38:29 +0200

    create tsconfig.json, convert LoadButton component to tsx, put onClick event on button element rather than its parent div and change id attribute names

---
+++ src/components/load-button/LoadButton.tsx
@@ -0,0 +1,22 @@
+import React, { MouseEventHandler } from 'react';
+import { useSettings } from '../../context/settings-context';
+
+type Props = {
+  text: string,
+  onClick: MouseEventHandler,
+  isDisplayed: boolean
+};
+
+export function LoadButton({ text, onClick, isDisplayed }: Props) {
+  const { darkModeIsActive } = useSettings();
+
+  const loadBtnClassName = darkModeIsActive ? 'dark-mode' : '';
+
+  return !isDisplayed ? null : (
+    <div id='load-button-container' className={loadBtnClassName}>
+      <button id='load-button' className={loadBtnClassName} onClick={onClick}>
+        <span id='load-button-text'>{text}</span>
+      </button>
+    </div>
+  );
+}
