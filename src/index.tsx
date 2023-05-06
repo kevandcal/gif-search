b09da6237cc9dd3fb8e13feaf6dd6fc6ddeec9a3
Kevin Callon <kevincallon@gmail.com>
2023-05-06 14:52:02 +0200

    complete typescript implementation in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './components/app/App';
 
-const root = createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+const root = createRoot(rootElement);
 root.render(<App />);
 
 
